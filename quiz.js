8d8d3c898e98fe3c2bca01f940dd352ce2127c9f
Sneha G R <grsneha73@gmail.com>
2025-10-15 10:20:35 +0530

    day3Node

---
+++ quiz.js
@@ -0,0 +1,12 @@
+let studentName = "Result";
+let correctAnswers = 4;
+let wrongAnswers = 1;
+
+function getFinalScore(name, correct, wrong) 
+{
+    return name === "Unknown" ? "Invalid Student" : (correct * 4) - (wrong * 1);
+}
+
+let result = getFinalScore(studentName, correctAnswers, wrongAnswers);
+console.log(result);
+console.log(typeof result);
